9275561d077e5e95c630d6f74f0d014c48b20ac2
Piotr Gorski <lucjan.lucjanov@gmail.com>
2021-09-02 17:21:33 +0200

    Revert "block, bfq: add support to track if root_group have any pending requests"
    This reverts commit 3bd122fe443c9f608379441e5144f33e7c1181cf.
    
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -967,14 +967,8 @@ reset_entity_pointer:
 void bfq_weights_tree_remove(struct bfq_data *bfqd,
 			     struct bfq_queue *bfqq)
 {
-	struct bfq_entity *entity = &bfqq->entity;
+	struct bfq_entity *entity = bfqq->entity.parent;
 
-	if (entity->in_groups_with_pending_reqs) {
-		entity->in_groups_with_pending_reqs = false;
-		bfqd->num_queues_with_pending_reqs_in_root--;
-	}
-
-	entity = entity->parent;
 	for_each_entity(entity) {
 		struct bfq_sched_data *sd = entity->my_sched_data;
 
